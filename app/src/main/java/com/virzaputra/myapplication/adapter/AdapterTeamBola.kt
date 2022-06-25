9639ee14d264a18b0d43f1743509a8eb905118cc
virzaptr <virzaputra20@mhs.pelitabangsa.ac.id>
2022-06-26 02:35:23 +0700

    RecyclerView APP

---
+++ app/src/main/java/com/virzaputra/myapplication/adapter/AdapterTeamBola.kt
@@ -0,0 +1,48 @@
+package com.virzaputra.myapplication.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.virzaputra.myapplication.databinding.ActivityMainBinding
+import com.virzaputra.myapplication.model.Pemain
+import com.virzaputra.myapplication.databinding.ListItemPemainBinding
+
+class AdapterTeamBola(private val context : Context,
+                      private var data : List<Pemain>?,
+                      private val itemclick : OnClickListener)
+                      : RecyclerView.Adapter<AdapterTeamBola.ViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListItemPemainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data?.get(position)
+        holder.foto.setImageResource(item?.foto ?:0)
+        holder.nama.text = item?.nama
+
+        holder.itemView.setOnClickListener {
+            itemclick.detailData(item)
+        }
+
+    }
+
+    override fun getItemCount(): Int = data?.size ?:0
+
+    inner class ViewHolder( val binding : ListItemPemainBinding): RecyclerView.ViewHolder(binding.root)  {
+        var foto = binding.image
+        var nama = binding.txtnama
+
+
+    }
+
+    interface OnClickListener {
+        fun detailData(item : Pemain?)
+    }
+
+                      }
